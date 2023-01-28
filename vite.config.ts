71af59c834207bd4da851ba6f3bca01ac03e4143
ahmadertarizqi <ahmadderta.rizqi30@gmail.com>
2023-01-28 15:53:35 +0700

    chore: site identity

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,27 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import * as path from 'path';
+import { createHtmlPlugin } from "vite-plugin-html";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: [
-      { find: '~', replacement: path.resolve(__dirname, 'src') }
-    ]
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    plugins: [
+      react(),
+      createHtmlPlugin({
+        minify: true,
+        inject: {
+          data: {
+            APP_TITLE: env.VITE_APP_TITLE
+          }
+        }
+      })
+    ],
+    resolve: {
+      alias: [
+        { find: '~', replacement: path.resolve(__dirname, 'src') }
+      ]
+    }
   }
 })
